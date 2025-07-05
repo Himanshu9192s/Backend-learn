38eccc69790bfb4b7cd438ac052095d07a313f8c
Himanshu9192s <officialhimanshusen@gmail.com>
2025-07-05 10:33:55 +0530

    github add data

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const mydata = {
+  name: 'Himanshu Sen',
+  age: 25,
+  city: 'Delhi',
+  country: 'India',
+  github: 'https://github.com/Himanshu9192s'
+
+}
+
+// app.get('/mydata01', (req, res) => {
+//   res.send(mydata)
+// })
+
+app.get('/mydata02', (req, res) => {
+  res.json(mydata)
+})
+
+app.get('/new', (req, res) => {
+  res.send(`This is a new route. My data: ${JSON.stringify(mydata)}`)
+})
+
+
 app.get('/himanshusen' ,(req, res) => {
     res.send(`<h1>Hello Himanshu Sen! using a ${port} </h1>`)
 })
